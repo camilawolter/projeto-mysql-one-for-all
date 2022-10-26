a0c109d445542e8dc085634819532ca4f20f0e28
Camila Wolter <camila0wolter@gmail.com>
2022-10-26 11:07:06 -0300

    requisito 5

---
+++ desafio5.sql
@@ -0,0 +1,9 @@
+SELECT 
+    c.cancao AS cancao, COUNT(h.cancao_id) AS reproducoes
+FROM
+    SpotifyClone.cancoes AS c
+        INNER JOIN
+    SpotifyClone.historico_reproducoes AS h ON c.cancao_id = h.cancao_id
+GROUP BY c.cancao
+ORDER BY reproducoes DESC , cancao
+LIMIT 2;
